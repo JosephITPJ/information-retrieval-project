7239b45262921e83dffd20ab9f45c52c295f4116
JosephITPJ <105132473+JosephITPJ@users.noreply.github.com>
2023-12-12 16:14:36 +0200

    Update phase2.py

--- document collection/phase2.py
+++ document collection/phase2.py
@@ -7,136 +7,146 @@ import pandas as pd
 import numpy as np
 import math
 
+#First phase 
 
-# Read files >>>>>>>>>>>
+
+#Read 10 files (.txt)  >>>>>>>>>>>
+documents = []
+
+file_names = natsorted(os.listdir("files"))
+
+
 def read_files(file):
     if 'txt' in file:
         with open(f'files/'+file, 'r') as f:
             return f.read()
-####Apply tokenization
+print("read 10 files.txt :\n")
+print(file_names,"\n")
+
+
+####Apply tokenization & stemmeing
 def preprocess_document(doc):
-    tokenized_doc = word_tokenize(doc)
+    tokenized_doc = word_tokenize(doc)  
     
     # Initialize the Porter Stemmer
     stemmer = PorterStemmer()
     
     # Apply stemming to each token
     stemmed_tokens = [stemmer.stem(token) for token in tokenized_doc]
+   
     
     return stemmed_tokens
-
-
+     
+    
 # Read files and preprocess each document
-documents = []
+
 for file in natsorted(os.listdir('files')):
     content = read_files(file)
     preprocessed_doc = preprocess_document(content)
+
     documents.append(preprocessed_doc)
 
 # Display the preprocessed documents>>>>>
+print("tokenization & stemming :\n")
+for doc in documents:
+    print(", ".join(doc))
+print("\n")
 
 
-#print(documents)
+
+# second phase 
 
 
 
-
-# Initialize the file no.
 fileno = 1
 
 # Initialize the dictionary.
 pos_index = {}
 
-file_names = natsorted(os.listdir("files"))
-#print(file_names)
 # For every file.
 for file_name in file_names:
-
     # Read file contents.
     with open(f'files/{file_name}', 'r') as f:
         doc = f.read()
-    # preprocess doc
+
+    # Preprocess doc.
     final_token_list = preprocess_document(doc)
 
     # For position and term in the tokens.
     for pos, term in enumerate(final_token_list):
-       # print(pos, '-->' ,term)
-        
         # If term already exists in the positional index dictionary.
         if term in pos_index:
-                
-            # Increment total freq by 1.
+         
             pos_index[term][0] = pos_index[term][0] + 1
-            
+
             # Check if the term has existed in that DocID before.
             if fileno in pos_index[term][1]:
                 pos_index[term][1][fileno].append(pos)
-                    
             else:
                 pos_index[term][1][fileno] = [pos]
 
         # If term does not exist in the positional index dictionary
         else:
-            
-            # Initialize the list.
+      
             pos_index[term] = []
-            # The total frequency is 1.
+       
             pos_index[term].append(1)
-            # The postings list is initially empty.
-            pos_index[term].append({})     
-            # Add doc ID to postings list.
+          
+            pos_index[term].append({})
+          
             pos_index[term][1][fileno] = [pos]
 
-    # Increment the file no. counter for document ID mapping             
+
     fileno += 1
 
-#print(pos_index)
+#>>>> print the positional index
+print("The posistional index\n") 
+for term, info in pos_index.items():
+    total_frequency, doc_positions = info
+  
+    print(f'{term}: [{total_frequency}, {doc_positions}]',)
 
+#######
 
 
 def put_query(q, display=1):
     lis = [[] for _ in range(10)]
     q = preprocess_document(q)
 
-    for term in q:
-       if term in pos_index.keys():
+    for i, term in enumerate(q):
+        if term in pos_index.keys():
             for key in pos_index[term][1].keys():
-            
                 if lis[key-1] != []:
-                    
-                    if lis[key-1][-1] == pos_index[term][1][key][0]-1:
+                    if lis[key-1][-1] == pos_index[term][1][key][0] - 1:
                         lis[key-1].append(pos_index[term][1][key][0])
+                    elif i > 0 and lis[key-1][-1] + 1 == pos_index[term][1][key][0]:
+                        lis[key-1][-1] = pos_index[term][1][key][0]
                 else:
                     lis[key-1].append(pos_index[term][1][key][0])
+
     positions = []
-    if display==1:
-        for pos, list in enumerate(lis, start=1):
-            if len(list) == len(q):
+    if display == 1:
+        for pos, lst in enumerate(lis, start=1):
+            if len(lst) == len(q):
                 positions.append('document '+str(pos))
         return positions
     else:
-        for pos, list in enumerate(lis, start=1):
-            if len(list) == len(q):
+        for pos, lst in enumerate(lis, start=1):
+            if len(lst) == len(q):
                 positions.append('doc'+str(pos))
         return positions
 
-q = 'fool fear'
-result = put_query(q)
-#print(result)
 
-
-
-## third phase  [Term frequency] 
+##   [Term frequency] 
 documents = []
 files = os.listdir('files')
 for file in range(1, 11):
     documents.append(" ".join(preprocess_document(read_files(str(file)+'.txt'))))
 
-all_terms = []
-for doc in documents:
-    for term in doc.split():
-        all_terms.append(term)
-all_terms = set(all_terms)
+# Get all unique terms in the order they appear in the original documents
+all_terms = [term for doc in documents for term in doc.split()]
+
+
 
 
 def get_tf(document):
@@ -150,8 +160,9 @@ for i in range(1, len(documents)):
     tf[i] = get_tf(documents[i]).values()
 tf.columns = ['doc'+str(i) for i in range(1, 11)]
 
-
-#print(tf)
+#print the term frequency
+print("\n<<<< Term frequency>>>\n")
+print(tf,"\n")
 
 
 
@@ -167,8 +178,9 @@ w_tf = tf.copy()
 for i in range(0, len(documents)):
     w_tf['doc'+str(i+1)] = tf['doc'+str(i+1)].apply(weighted_tf)
 
-
-#print(w_tf)
+## print the idf 
+print("display the Weighted_tf\n")
+print(w_tf,"\n")
 
 #compute tdf.idf 
 
@@ -182,15 +194,17 @@ for i in range(len(tf)):
 
 tdf.index=w_tf.index
 
+## print df&idf
+print("calculate df&idf\n")  
+print(tdf,"\n")
 
-#print(tdf)
 
 #tf*idf
 
 
 tf_idf = w_tf.multiply(tdf['idf'], axis=0)
 
-#print(tf_idf)
+print(tf_idf)
 
 # normailzed tf.idf
 def get_doc_len(col):
@@ -201,7 +215,8 @@ for col in tf_idf.columns:
     doc_len.loc[0, col+'_length']= get_doc_len(col)
 
 ##compute document length 
-#print(doc_len)
+print("calculate document length\n")  
+print(doc_len,"\n")
 
 def get_norm_tf_idf(col, x):
     try:
@@ -214,7 +229,8 @@ norm_tf_idf = pd.DataFrame()
 for col in tf_idf.columns:
     norm_tf_idf[col] = tf_idf[col].apply(lambda x : get_norm_tf_idf(col, x))
 
-#print(norm_tf_idf)
+print("calculate normalized tf.idf \n")
+print(norm_tf_idf,"\n")
 
 #
 def get_w_tf(x):
@@ -224,11 +240,19 @@ def get_w_tf(x):
         return 0
 
 #insert query >>>>>>>
+
+
+
+
+
 def insert_query(q):
+
     docs_found = put_query(q, 2)
     if docs_found == []:
         return "Not Fount"
+    
     new_q = preprocess_document(q)
+  
     query = pd.DataFrame(index=norm_tf_idf.index)
     query['tf'] = [1 if x in new_q else 0 for x in list(norm_tf_idf.index)]
     query['w_tf'] = query['tf'].apply(lambda x : get_w_tf(x))
@@ -241,6 +265,7 @@ def insert_query(q):
     print('Query Details')
     print(query.loc[new_q])
     product2 = product.multiply(query['normalized'], axis=0)
+    # Calculate similarity scores
     scores = {}
     for col in put_query(q, 2):
             scores[col] = product2[col].sum()
@@ -259,10 +284,23 @@ def insert_query(q):
     print('Cosine Simliarity')
     print(product_result.sum())
     print()
+      # Sort scores in descending order
     sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    # Print ranked documents
     print('Returned docs')
     for typle in sorted_scores:
         print(typle[0], end=" ")
 
 
-insert_query('antony brutus')
+
+
+
+
+
+while True:
+    user_query = input("Enter your query (): ")
+    
+   
+    # Process the user's query
+    
+    insert_query(user_query)
